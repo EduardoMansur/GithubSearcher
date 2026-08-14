1e61977a9e7420c8966b9e5588224f4fc26b7a21
Eduardo Torres Mansur Pereira <eduardo.torresm@hotmail.com>
2026-08-14 08:48:05 -0300

    feat: initial commit

---
+++ GithubSearcher/GithubSearcher/GithubSearcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSearcherApp.swift
+//  GithubSearcher
+//
+//  Created by Eduardo Torres Mansur Pereira on 14/08/26.
+//
+
+import SwiftUI
+
+@main
+struct GithubSearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
